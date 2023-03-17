e7cd76529c982b2ce4da84a24d6e91d52f5f5dcb
Philip Keicher <philip.keicher@cern.ch>
2023-03-17 14:13:57 +0100

    continued work on script, WIP: move output to remote target

--- run_missing_crabjobs_locally.py
+++ run_missing_crabjobs_locally.py
@@ -3,7 +3,9 @@ import sys
 import json
 
 from tqdm import tqdm
+from datetime import datetime
 from argparse import ArgumentParser, RawDescriptionHelpFormatter
+from subprocess import call
 
 thisdir = os.path.realpath(os.path.dirname(__file__))
 
@@ -14,18 +16,140 @@ import wlcg_dbs_interface as interface
 
 verbosity = 0
 
+def run_custom_nano_command(
+    input_file: str,
+    nevents: int=100,
+    era: str="Run2_2017"
+):
+    # first build the cms PSet
+    cmd = [f'python3 {thisdir}/RunKit/nanoProdWrapper.py']
+    cmd += [f'customise=Framework/NanoProd/customiseNano.customise'] 
+    cmd += [f'skimCfg={thisdir}/config/skim.yaml']
+    cmd += [f'maxEvents={nevents}'] 
+    cmd += [f'sampleType=mc'] 
+    cmd += [f'storeFailed=True'] 
+    cmd += [f'era={era}'] 
+    cmd += [f'inputFiles=file:{input_file}']
+    print(f"creating PSet.py for input '{input_file}'")
+    call([" ".join(cmd)], shell=True)
+    cmd=f'{thisdir}/RunKit/nanoProdCrabJob.sh'
+
+    print("executing job")
+    call([cmd], shell=True)
+
+def run_job(
+    lfn: str,
+    tmp_dir: str,
+    wlcg_path:str,
+    output_name:str,
+    **kwargs,
+):
+    # cd into tmp dir. If it doesn't exist, create it
+    if not os.path.exists(tmp_dir):
+        os.makedirs(tmp_dir)
+    cwd = os.getcwd()
+    os.chdir(tmp_dir)
+
+    # copy lfn locally
+    interface.get_remote_file(
+        filepath=lfn,
+        target=output_name
+    )
+    if not os.path.exists(output_name):
+        raise ValueError(f"Unable to load '{lfn}'")
+
+    # run the job with this LFN
+    run_custom_nano_command(
+        input_file=output_name,
+
+    )
+    # copy the output to the (remote) WLCG site
+    from IPython import embed; embed()
+
+    # change back to the original directory after we're done
+    os.chdir(cwd)
+
+
+def build_wlcg_path(
+    wlcg_prefix: str,
+    wlcg_dir: str,
+    sample_name: str,
+    crab_dirname: str,
+    time_stamp: str,
+    job_output: str,
+    wlcg_template: str=os.path.join("{wlcg_prefix}{wlcg_dir}",
+        "{sample_name}",
+        "{crab_dirname}",
+        "{time_stamp}",
+        "{job_output}"
+    ),
+):
+    return wlcg_template.format(
+        wlcg_prefix=wlcg_prefix,
+        wlcg_dir=wlcg_dir,
+        sample_name=sample_name,
+        crab_dirname=crab_dirname,
+        time_stamp=time_stamp,
+        job_output=job_output,
+    )
 
 def main(
     *args, 
+    wlcg_prefix: str,
+    wlcg_dir: str,
     missing_files_json: str,
+    sample_config: str,
+    veto_dirs: list[str]=None,
+    tmp_dir: str="./tmp",
+    remote_dir: str="manual_jobs",
     **kwargs,
 ):
+    if not veto_dirs:
+        veto_dirs=list()
     # load dictionary with missing lfns
     missing_lfn_dict = dict()
     with open(missing_files_json) as f:
         missing_lfn_dict = json.load(f)
     
-    pbar_samples = tqdm(missing_lfn_dict.keys())
+    # filter out samples that are accounted for in the list of veto directories
+    missing_samples = list(filter(
+        lambda x: not any(dir.endswith(x) for dir in veto_dirs),
+        missing_lfn_dict
+    ))
+    pbar_samples = tqdm(missing_samples)
+    # create a time stamp that mimics the crab format
+    timestamp = '{:%y%m%d_%H%M%S}'.format(datetime.now())
+
+    for sample in pbar_samples:
+        pbar_samples.set_description(f"Run missing jobs for sample '{sample}'")
+
+        # load das key for this sample
+        das_key = interface.load_das_key(
+            sample_name=sample,
+            sample_config=sample_config
+        )
+
+        # get campaign name
+        sample_campaign = interface.get_campaign_name(das_key=das_key)
+        
+        # loop through the list of missing lfns
+        pbar_missing_lfns = tqdm(missing_lfn_dict[sample]["missing_lfns"])
+        pbar_missing_lfns.set_description("Running LFN")
+        for i, lfn in enumerate(pbar_missing_lfns):
+            wlcg_path = build_wlcg_path(
+                wlcg_prefix=wlcg_prefix,
+                wlcg_dir=wlcg_dir,
+                sample_name=sample_campaign,
+                crab_dirname=remote_dir,
+                time_stamp=timestamp,
+                job_output=f"{i:04d}"
+            )
+            run_job(
+                lfn=lfn,
+                tmp_dir=tmp_dir,
+                wlcg_path=wlcg_path,
+                output_name=f"nano_{i}.root",
+            )
     
 
 def parse_arguments():
@@ -91,6 +215,21 @@ def parse_arguments():
     )
     
     parser.add_argument(
+        "-r", "--remote-dir",
+        help=" ".join(
+            """
+                safe the output of the jobs you run locally in this
+                directory in the (remote) WLCG site. The path on the remote
+                site is formed as 
+                '{wlcg-dir}/{sample_campaign}/{remote-dir}/{time-stamp}'
+            """.split()
+        ),
+        type=str,
+        default="manual_jobs",
+        dest="remote_dir",
+    )
+
+    parser.add_argument(
         "--sample-config", "-c",
         help=" ".join(
             """
@@ -143,6 +282,19 @@ def parse_arguments():
         dest="tmp_dir",
         metavar="path/to/temporary_directory",
         default="./tmp"
+    )
+
+    parser.add_argument(
+        "veto-directories",
+        help=" ".join(
+            """
+                veto samples in the missing files json that are accounted
+                for in this list. 
+            """.split()
+        ),
+        metavar="path/to/crab_dirs",
+        nargs="+"
+        # dest="veto_dirs"
     )
 
     args = parser.parse_args()
